4c05446b539946ce2003b14904bbd01ba7cf67b7
LITTLEYUEBABYCO\LittleBaby <1>
2024-06-01 22:32:57 +0800

    修改+初始化动作

---
+++ src/main/kotlin/com/automain/colorfulbrackets/BracketFinder.kt
@@ -0,0 +1,44 @@
+﻿package com.automain.colorfulbrackets
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor
+import java.util.*
+
+object BracketFinder {
+    fun findBrackets(project: Project, file: PsiFile) {
+        if (file is PsiFile) {
+            val stack = Stack<PsiElement>()
+            val pairs = mutableListOf<Pair<PsiElement, PsiElement>>()
+
+            file.accept(object : PsiRecursiveElementWalkingVisitor() {
+                override fun visitElement(element: PsiElement) {
+                    super.visitElement(element)
+                    when (element.text) {
+                        "{" -> {
+                            stack.push(element)
+                        }
+                        "}" -> {
+                            if (stack.isNotEmpty()) {
+                                val openingBracket = stack.pop()
+                                pairs.add(openingBracket to element)
+                            } else {
+                                println("Unmatched closing bracket at ${element.textRange}")
+                            }
+                        }
+                    }
+                }
+            })
+
+            if (stack.isNotEmpty()) {
+                println("Unmatched opening brackets:")
+                stack.forEach { println("Opening bracket at ${it.textRange}") }
+            }
+
+            pairs.forEach { (open, close) ->
+                println("Found bracket pair: (${open.textRange}, ${close.textRange})")
+            }
+        }
+    }
+}
