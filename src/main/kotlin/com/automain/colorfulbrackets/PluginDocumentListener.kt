9a4b8ae39333891e78ca1f0bd67fdb7001222902
AutoMain-t <444957308@qq.com>
2024-06-07 00:11:52 +0800

    配置修改

--- src/main/kotlin/com/automain/colorfulbrackets/PluginDocumentListener.kt
+++ src/main/kotlin/com/automain/colorfulbrackets/PluginDocumentListener.kt
@@ -1,13 +1,13 @@
 ﻿package com.automain.colorfulbrackets
 
-import com.intellij.openapi.application.ApplicationManager
+//import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.editor.Document
+//import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
+//import com.intellij.psi.PsiDocumentManager
 
 //文档修改
 @Service(Service.Level.PROJECT)
