3a4499f06d13234ba224653d3fcf557f2f886612
AutoMaintyc <57486158+AutoMaintyc@users.noreply.github.com>
2024-06-12 11:21:52 +0800

    Debug环境修改，用Rider作为测试环境

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2023.1.1")
-    type.set("IC") // Target IDE Platform
+    type.set("RD") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
 }
