789933c22e794831165fa0c5c3ae2377f11f3541
tongyachao <tongyachao>
2024-11-18 19:02:17 +0800

    存档

---
+++ src/main/kotlin/com/automain/colorfulbrackets/BracketHelper.kt
@@ -0,0 +1,64 @@
+﻿package com.automain.colorfulbrackets
+
+import com.automain.colorfulbrackets.type.BracketType
+import com.intellij.codeInsight.highlighting.BraceMatchingUtil
+import com.intellij.lang.Language
+import com.intellij.lang.LanguageBraceMatching
+import com.intellij.lang.LanguageExtension
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.ui.JBColor
+import java.awt.Color
+import kotlin.random.Random
+
+object BracketHelper {
+    private val bracketType = LanguageExtension<BracketType>("com.AutoMain.ColorfulBrackets.bracketType")
+
+    fun needCheck(element: PsiElement): Boolean {
+        if (isRightBracket(element)) return false
+        return element is LeafPsiElement
+    }
+
+    fun findRightBracket(element: PsiElement): PsiElement {
+        /*根据给定元素向右查找符合type的element*/
+        val languages = Language.getRegisteredLanguages()
+        for (language in languages) {
+            val pairs = LanguageBraceMatching.INSTANCE.forLanguage(language)?.pairs.let {
+                if (it.isNullOrEmpty()) {
+                    language.associatedFileType
+                        ?.let { fileType ->
+                            BraceMatchingUtil.getBraceMatcher(
+                                fileType,
+                                language
+                            ) as? PairedBraceMatcher
+                        }
+                        ?.pairs
+                } else {
+                    it
+                }
+            }
+            val pairsList = bracketType.forLanguage(language)?.pairs()?.let {
+                if (!pairs.isNullOrEmpty()) {
+                    it.toMutableSet().apply { addAll(pairs) }
+                } else {
+                    it
+                }
+            } ?: pairs?.toList()
+            println(pairsList)
+        }
+
+        return element.parent
+    }
+
+    private fun isRightBracket(element: PsiElement): Boolean {
+        return false
+    }
+
+    fun getRandomColor(): JBColor {
+        val red = Random.nextInt(50, 256)
+        val green = Random.nextInt(50, 256)
+        val blue = Random.nextInt(50, 256)
+        return JBColor(Color(red, green, blue), Color(red, green, blue))
+    }
+}
---
+++ src/main/kotlin/com/automain/colorfulbrackets/visitor/PluginHighlightVisitorDefault.kt
@@ -0,0 +1,46 @@
+﻿package com.automain.colorfulbrackets.visitor
+
+import com.automain.colorfulbrackets.BracketHelper
+import com.intellij.codeInsight.daemon.impl.HighlightVisitor
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightInfoHolder
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+
+class PluginHighlightVisitorDefault : HighlightVisitor {
+    private var highlightInfoHolder: HighlightInfoHolder? = null
+
+    override fun suitableForFile(file: PsiFile): Boolean {
+        return PropertiesComponent.getInstance().getBoolean("{}")
+    }
+
+    override fun analyze(
+        file: PsiFile,
+        updateWholeFile: Boolean,
+        holder: HighlightInfoHolder,
+        action: Runnable
+    ): Boolean {
+        highlightInfoHolder = holder
+        try { action.run() }
+        catch (e: Throwable) { println(e.toString()) }
+        finally { highlightInfoHolder = null; }
+        return true
+    }
+
+
+    override fun clone(): HighlightVisitor {
+        return this
+    }
+
+    override fun visit(element: PsiElement) {
+        /*在这里找到需要高亮的*/
+        if (!BracketHelper.needCheck(element)) return
+        var rightBracket = BracketHelper.findRightBracket(element)
+        SetHightlight(element)
+        SetHightlight(rightBracket)
+    }
+
+    protected fun SetHightlight(element: PsiElement){
+        /*设置高亮信息*/
+    }
+}
